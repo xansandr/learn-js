4cb346c91ace58aa01c075d9f439e8587f7639b1
Alexandr_Korobeinykov <xansandr@yandex.by>
2024-02-29 22:27:29 +0300

    isolated variables from global scope

--- HW1/js/task_8.js
+++ HW1/js/task_8.js
@@ -1,11 +1,11 @@
-var a = 8,
+{let a = 8,
   b = 3;
 
-var x = (16 - a - 2 * b) / 2;
+let x = (16 - a - 2 * b) / 2;
 console.log("task 8:", x);
 
-var x = (a - 90) / (b - 6);
+x = (a - 90) / (b - 6);
 console.log(x);
 
-var x = 23780 / (3 + a + b);
-console.log(x);
+x = 23780 / (3 + a + b);
+console.log(x);}
