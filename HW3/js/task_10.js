d2dc4dc0640d02ebfc82dc8b3c6b01aa013c07aa
Alexandr_Korobeinykov <xansandr@yandex.by>
2024-03-20 22:21:42 +0300

    finished HW3, fixed hw3/task_10

--- HW3/js/task_10.js
+++ HW3/js/task_10.js
@@ -1,14 +1,16 @@
 {
   let n = 1209,
     numbers = String(n).split("");
-  let numbersLenght = numbers.length;
+  let numbersLength = numbers.length;
   let summ = 0;
-  for (let i = 0; i < numbersLenght; i++) {
+  for (let i = 0; i < numbersLength; i++) {
     summ += Number(numbers[i]);
   }
   let reverse = "";
-  for (let i = numbersLenght - 1; i >= 0 ; i--) {
+  for (let i = numbersLength - 1; i >= 0; i--) {
     reverse += numbers[i];
   }
-console.log( ` а. ${numbers}\n\n b. ${numbersLenght}\n\n c. ${summ}\n\n d. ${reverse}`)
+  console.log(
+    ` а. ${numbers}\n\n b. ${numbersLength}\n\n c. ${summ}\n\n d. ${reverse}`
+  );
 }
