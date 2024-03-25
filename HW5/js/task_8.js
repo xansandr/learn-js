525dbb5aa33287c6efdb7476f18eed0d98cf3cdc
Alexandr_Korobeinykov <xansandr@yandex.by>
2024-03-25 07:13:20 +0300

    additional task hw8 complete

--- HW5/js/task_8.js
+++ HW5/js/task_8.js
@@ -16,4 +16,23 @@
     }
   }
   treygolnik((userInput = prompt("Введите число")));
+  console.log("");
+
+  function reversetreygolnik(userInput = prompt("Введите число")) {
+    let star = "*",
+      str = star + star,
+      whiteSpace = " ",
+      amountSpace = "";
+
+    for (j = userInput - 1; j >= 0; j--) {
+      for (let i = 0; i < userInput - j; i++) {
+        amountSpace = whiteSpace.repeat(i);
+      }
+      for (let i = 0; i < userInput; i++) {
+        mnozh = str.repeat(j);
+      }
+      console.log(amountSpace + (star + mnozh));
+    }
+  }
+  reversetreygolnik((userInput = prompt("Введите число")));
 }
