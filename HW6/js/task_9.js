bc4a673e26ec7cdcb7abae05936bd2dea6a3aa42
Aliaksandr Karabeinikau <xansandr@yandex.by>
2024-04-21 17:11:57 +0300

    fixed 3rd condition, added test for each one

--- HW6/js/task_9.js
+++ HW6/js/task_9.js
@@ -1,5 +1,8 @@
 {
-  let mail = "dada132ysa@mail.lol.com";
+  let mail = "dadaya123@mail.lol.com",
+    mail1 = "dada+ya123@mail.lol.com",
+    mail2 = "1dadaya123@mail.lol.com",
+    mail3 = "dadaya123@mail.lol.communismdelopravoe";
 
   function validateMail(mail) {
     let firRegex = /^[a-zA-Z0-9_.\-]*[@]?[a-zA-Z0-9_.\-]*$/;
@@ -12,7 +15,7 @@
 
     // console.log("2 condition - " + secCondition);
 
-    let thiRegex = /.[a-zA-Z0-9_\-]{2,11}$/;
+    let thiRegex = /[\.][a-zA-Z0-9_\-]{2,11}?$/;
     let thiCondition = thiRegex.test(mail);
 
     // console.log("3 condition - " + thiCondition);
@@ -22,4 +25,7 @@
     } else return console.log(false);
   }
   validateMail(mail);
+  validateMail(mail1);
+  validateMail(mail2);
+  validateMail(mail3);
 }
