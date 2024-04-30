ce08442700699f6b3fb23d5ea89e7d6e546e0507
Aliaksandr Karabeinikau <xansandr@yandex.by>
2024-05-01 01:16:22 +0300

    HW7 finished

--- HW7/js/calculator.js
+++ HW7/js/calculator.js
@@ -2,16 +2,23 @@
   function isNumber(value) {
     return typeof value === "number";
   }
+  let regexToggleOff = "выключить";
+  let regexToggleOn = "включить";
 
-  console.log("Калькулятор выключен");
+  function statusOff() {
+    console.log("Калькулятор выключен");
+  }
 
   let calculator = {
     status: false,
 
     toggleStatus: function () {
       if (this.status == false) {
-        toggleInput = prompt("Включите калькулятор", "Включить");
-        if (toggleInput == "Включить") {
+        toggleInput = prompt(
+          'Включите калькулятор (чтобы выключить каркулятор в любой момент введите "Выключить"',
+          "Включить"
+        ).toLowerCase();
+        if (toggleInput === "включить") {
           this.status = true;
           console.log("Калькулятор включен");
           calculator.operationInput();
@@ -19,24 +26,39 @@
       }
     },
     operationInput: function () {
-      let regex = /[0-9]+/g;
+      let regexNum = /[0-9]+/;
+
       if (this.status == true) {
-        let firstNumInput = Number(prompt("Введите первое число")),
-          operatorInput = prompt("Введите оператор вычисления"),
-          secondNumInput = Number(prompt("Введите второе число"));
-        console.log(firstNumInput);
-        console.log(secondNumInput);
-        console.log(regex.test(firstNumInput));
-        console.log(regex.test(secondNumInput));
-        if (regex.test(firstNumInput) && regex.test(secondNumInput)) {
-          let formula = firstNumInput + operatorInput + secondNumInput,
+        let firstNumInput = prompt("Введите первое число").toLowerCase();
+        if (firstNumInput === "выключить") {
+          this.status = false;
+          statusOff();
+          return this.toggleStatus();
+        }
+        let operatorInput = prompt("Введите оператор вычисления").toLowerCase();
+        if (operatorInput === "выключить") {
+          this.status = false;
+          statusOff();
+          return this.toggleStatus();
+        }
+        let secondNumInput = prompt("Введите второе число").toLowerCase();
+        if (secondNumInput === "выключить") {
+          this.status = false;
+          statusOff();
+          return this.toggleStatus();
+        }
+        if (regexNum.test(firstNumInput) && regexNum.test(secondNumInput)) {
+          let formula =
+              Number(firstNumInput) + operatorInput + Number(secondNumInput),
             //   То, что вы видите ниже - буквально игрушка дьявола, эта штука буквально практически выполняет функционал инженерного калькулятора, может считать побитовые операции, сдвига. Я бы мог конечно просто прописать через case возможные операции для этого псевдокалькулятора, но.. Так ведь веселее?
 
-            answer = eval(formula);D
+            answer = eval(formula);
 
-          //
           console.log(formula + "=" + answer);
         } else console.log("Введенное значение не является числом");
+        console.log(firstNumInput, operatorInput, secondNumInput);
+
+        this.operationInput();
       }
     },
   };
